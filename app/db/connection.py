7b8040d63e75b4a68bdfed83ee41decf9c0c83ac
Erykah <erkahwanga@gmail.com>
2025-05-28 10:19:21 +0300

    implement database connection and session management

---
+++ app/db/connection.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine('sqlite:///../../interior_design.db')
+Session = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def get_session():
+    return Session()
